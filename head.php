61d669e66e0a1f97752e2145e69e9d60e594e12d
MichalPacesa <michal.pacesa4@gmail.com>
2024-08-23 01:30:28 +0200

    Update CSS file paths in head.php

--- head.php
+++ head.php
@@ -6,31 +6,29 @@
 <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 		
-<!-- font awesome -->
 <!-- bootstrap -->
-
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+
 <!-- mdbootstrap -->
 <link rel="stylesheet" href="mdbootstrap/css/mdb.min.css" />
 <link rel="stylesheet" href="mdbootstrap/css/all.css">
+
  <!-- Google Fonts Roboto -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
 <!-- MDB script-->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.umd.min.js" ></script>
-
 
 <!-- CSS štýl -->
 
 <style>
-    <?php include "style.css"; ?>
-    <?php include "style_rd.css"; ?>
-    <?php include "style_roboto.css"; ?>
-
+    <?php include "css/style.css"; ?>
+    <?php include "css/style_rd.css"; ?>
+    <?php include "css/style_roboto.css"; ?>
 </style>
 	
-
 <script>
 	/* po kliknuti na ikonku mobilneho menu - zobrazi alebo zmaze mobilne menu*/
 	function menuPreMobily(){
@@ -44,7 +42,6 @@
 		
 	}
 </script>
-
 
  <script>
 function ShowPassword() {
