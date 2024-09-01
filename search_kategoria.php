f987696fa1a0de6b395e171097634725b3a2c872
MichalPacesa <michal.pacesa4@gmail.com>
2024-09-01 18:28:50 +0200

    Oprava pri vypise chybovej hlasky

--- search_kategoria.php
+++ search_kategoria.php
@@ -1,12 +1,12 @@
 <?php
-// program na kontrolu existencie stroja. Vysledok posle ako json a hlaska sa zobrazi sa v message
+// program na kontrolu existencie kategorie. Vysledok posle ako json a hlaska sa zobrazi sa v message
 include "config.php";
 include "lib.php";
 $hladaj=$_GET["q"];
 $sql = "SELECT KategoriaID FROM kategoria WHERE Kat_nazov='$hladaj'";
 $vysledok = mysqli_query($dblink, $sql);
 if (!$vysledok){
-    $chyba= "Chyba pri vyhladani stroja <br>";
+    $chyba= "Chyba pri vyhladani kategorie <br>";
     echo json_encode($chyba);
 }
 else {
