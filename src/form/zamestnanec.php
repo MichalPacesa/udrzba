13af953e052927a8d6a6e80319a060cfdc08c11b
MichalPacesa <michal.pacesa4@gmail.com>
2024-09-06 00:30:13 +0200

    Uprava cesty k zmena_zamestnanca

--- src/form/zamestnanec.php
+++ src/form/zamestnanec.php
@@ -150,7 +150,7 @@ else
 <h1><?php echo $nadpis; ?></h1>
 
 <p>
-<form id ="myapp_form" action="zmena_zamestnanca.php" method="POST" onsubmit="return app.usernameChecked">
+<form id ="myapp_form" action="../zmena/zmena_zamestnanca.php" method="POST" onsubmit="return app.usernameChecked">
 
     <p class="oznam text-grey text-start">Položky označené <span class="red bold">*</span> sú povinné</p>
 
@@ -305,7 +305,7 @@ else
     </table>
 </form>
 
-<form id="back" action="zmena_zamestnanca.php" method="POST"></form>
+<form id="back" action="../zmena/zmena_zamestnanca.php" method="POST"></form>
 </p>
 <?php
 mysqli_close($dblink); // odpojit sa z DB
