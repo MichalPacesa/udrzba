9ba646d341d572b385365a04c1d6c4a5762305fc
MichalPacesa <michal.pacesa4@gmail.com>
2024-09-06 00:27:59 +0200

    Vymazanie poznamok v logout

--- src/auth/logout.php
+++ src/auth/logout.php
@@ -5,7 +5,6 @@ unset($_SESSION['Login_ID_pouzivatela']);
 unset($_SESSION['Login_Prihlasovacie_meno']); 
 unset($_SESSION['Login_Meno_Priezvisko']);
 unset($_SESSION['Login_RolaID']);
-//session_destroy();
 header('Location: ../../index.php');
 ?>
 
